231a5248961e01d2af6bec69b4bb5559bfc79117
Tonya-Shyva <toshash69@gmail.com>
2023-03-31 09:02:40 +0300

    created animate Loader
    choosed "Watch" from react-spinners library, associate with cooking time.

--- src/Components/ui/Loader/Loader.jsx
+++ src/Components/ui/Loader/Loader.jsx
@@ -1,5 +1,19 @@
+import { Watch } from "react-loader-spinner";
+import LoaderWrapper from "./Loader.styled";
+
 const Loader = () => {
-  return <p>Loading...</p>;
+  return (
+    <LoaderWrapper>
+      <Watch
+        height="150"
+        width="150"
+        radius="48"
+        color="#8BAA36"
+        ariaLabel="watch-loading"
+        visible={true}
+      />
+    </LoaderWrapper>
+  );
 };
 
 export default Loader;
---
+++ src/Components/ui/Loader/Loader.styled.js
@@ -0,0 +1,11 @@
+import styled from "styled-components";
+
+const LoaderWrapper = styled.div`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  width: 100vw;
+  height: 100vh;
+`;
+
+export default LoaderWrapper;
