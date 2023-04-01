436998fdda1d123c72069283c8504def0edb6480
Reqvite <reqvite2233@gmail.com>
2023-04-01 16:21:00 +0300

    upd

---
+++ src/redux/shopping/shoppingSlice.js
@@ -0,0 +1,53 @@
+import { createSlice } from "@reduxjs/toolkit";
+
+const initialState = {
+  list: [],
+  isLoading: false,
+  error: null,
+};
+
+const contactsSlice = createSlice({
+  name: "contacts",
+  initialState,
+  extraReducers: (builder) =>
+    builder
+      //   .addCase(fetchContacts.pending, (state, action) => {
+      //     state.isLoading = true;
+      //   })
+      //   .addCase(fetchContacts.fulfilled, (state, action) => {
+      //     state.isLoading = false;
+      //     state.error = null;
+      //     state.items = action.payload;
+      //   })
+      //   .addCase(fetchContacts.rejected, (state, action) => {
+      //     state.isLoading = false;
+      //     state.isLoading = false;
+      //     state.error = action.payload;
+      //   })
+      //   .addCase(addContact.pending, (state, action) => {
+      //     state.isLoading = true;
+      //   })
+      //   .addCase(addContact.fulfilled, (state, action) => {
+      //     state.isLoading = false;
+      //     state.items.push(action.payload);
+      //   })
+      //   .addCase(addContact.rejected, (state, action) => {
+      //     state.isLoading = false;
+      //     state.error = action.payload;
+      //   })
+      .addCase(deleteContact.pending, (state, action) => {})
+      .addCase(deleteContact.fulfilled, (state, action) => {
+        state.isLoading = false;
+        state.error = null;
+        const idx = state.items.findIndex(
+          (contact) => contact.id === action.payload.id
+        );
+        state.items.splice(idx, 1);
+      })
+      .addCase(deleteContact.rejected, (state, action) => {
+        state.isLoading = false;
+        state.error = action.payload;
+      }),
+});
+
+export const contactsReduser = contactsSlice.reducer;
