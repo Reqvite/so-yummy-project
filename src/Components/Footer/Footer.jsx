5fd9f35febd0b8b036a7222af6e2cc933465b2f3
Yurii PErekrestnyi <jodlei1618@gmail.com>
2023-04-03 16:56:22 +0300

    footer

--- src/Components/Footer/Footer.jsx
+++ src/Components/Footer/Footer.jsx
@@ -1,5 +1,23 @@
+import styled from "styled-components";
+// import img from "";
+
 const Footer = () => {
-  return <div>Footer</div>;
+  return (
+    <div>
+      Footer
+      <GreenLeafs></GreenLeafs>
+    </div>
+  );
 };
 
+const GreenLeafs = styled.div`
+  position: absolute;
+  top: -240px;
+  left: 0;
+  width: 320px;
+  height: 240px;
+  background-image: url();
+  background-repeat: no-repeat;
+  background-size: contain;
+`;
 export default Footer;
