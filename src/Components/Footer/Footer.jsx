007552cfaa43d0c19f46ee8a27bf983a3038af40
Yurii PErekrestnyi <jodlei1618@gmail.com>
2023-04-03 21:57:15 +0300

    add images to footer

--- src/Components/Footer/Footer.jsx
+++ src/Components/Footer/Footer.jsx
@@ -1,23 +1,31 @@
 import styled from "styled-components";
-// import img from "";
+import imgTop1 from "../../assets/images/Footer/BackgroundImageTop/leafs-footer-mob.png";
 
 const Footer = () => {
   return (
-    <div>
-      Footer
-      <GreenLeafs></GreenLeafs>
-    </div>
+    <FooterWrap>
+      <GreenLeafsTop></GreenLeafsTop>
+      <FooterBottomWrap></FooterBottomWrap>
+    </FooterWrap>
   );
 };
 
-const GreenLeafs = styled.div`
+const FooterWrap = styled.footer`
+  position: relative;
+`;
+
+const GreenLeafsTop = styled.div`
   position: absolute;
   top: -240px;
   left: 0;
   width: 320px;
   height: 240px;
-  background-image: url();
+  background-image: url(${imgTop1});
   background-repeat: no-repeat;
   background-size: contain;
+  z-index: -1;
 `;
+
+const FooterBottomWrap = styled.div``;
+
 export default Footer;
