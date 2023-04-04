94c847482e71deda10dd475201a5916d4f5ef4e4
Reqvite <reqvite2233@gmail.com>
2023-04-04 16:52:39 +0300

    Update SearchPage.jsx

--- src/Pages/SearchPage.jsx
+++ src/Pages/SearchPage.jsx
@@ -1,7 +1,6 @@
 import { PageWrapper } from "Components/CategoriesList/CategoriesList.styled";
 import SelectSearch from "Components/SelectSearch/SelectSearch";
 import ImageBackground from "Components/common/ImageBackground/ImageBackground";
-import PageContainer from "Components/common/PageContainer/PageContainer";
 import { Title } from "Components/ui/MainPageTitle/MainPageTitle.styled";
 
 // import SearchRecipesList from "Components/SearchRecipesList/SearchRecipesList";
@@ -10,18 +9,16 @@ import SearchForm from "Components/ui/SearchForm/SearchForm";
 
 const SearchPage = () => {
   return (
-    <main>
+    <>
       <PageWrapper>
         <ImageBackground />
-        <PageContainer>
-          <Title margBottom="50px">Search</Title>
-          <SearchForm />
-          <SelectSearch />
+        <Title margBottom="50px">Search</Title>
+        <SearchForm />
+        <SelectSearch />
 
-          {/* <SearchRecipesList query="" /> */}
-        </PageContainer>
+        {/* <SearchRecipesList query="" /> */}
       </PageWrapper>
-    </main>
+    </>
   );
 };
 
