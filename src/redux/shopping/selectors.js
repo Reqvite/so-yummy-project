f7ec33266b36311b17c5f2560109bb4b3debdf06
Reqvite <reqvite2233@gmail.com>
2023-04-05 13:00:52 +0300

    delete container rotated blocks from main, recipe page

--- src/redux/shopping/selectors.js
+++ src/redux/shopping/selectors.js
@@ -1,2 +1,4 @@
 export const selectList = (state) => state.shopping.list;
 export const selectIsLoading = (state) => state.shopping.isLoading;
+export const selectShoppingListUpateIsLoading = (state) =>
+  state.shopping.shoppingListUpateIsLoading;
