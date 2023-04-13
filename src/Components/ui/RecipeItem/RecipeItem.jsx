f8e33c94c29889d6971edd2dd8f6661d32a00208
Reqvite <reqvite2233@gmail.com>
2023-04-14 01:04:35 +0300

    Update RecipeItem.jsx

--- src/Components/ui/RecipeItem/RecipeItem.jsx
+++ src/Components/ui/RecipeItem/RecipeItem.jsx
@@ -24,7 +24,7 @@ const RecipeItem = ({ _id, description, preview, time, title }) => {
   const isMobile = useMediaQuery("(max-width:767px)");
   const themeSelect = useSelector(selectTheme);
 
-  const newTitle = isMobile ? title.substring(0, 20) + "..." : title;
+  const newTitle = isMobile ? title?.substring(0, 20) + "..." : title;
 
   // const editedDescripiton = description.split(/[.?!]/);
 
