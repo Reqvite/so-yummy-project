11fcde0eab3a59719bb1d264c7029f63110c83eb
Reqvite <reqvite2233@gmail.com>
2023-04-13 16:42:04 +0300

    fix font

--- src/Components/GlobalStyle/GlobalStyle.jsx
+++ src/Components/GlobalStyle/GlobalStyle.jsx
@@ -3,14 +3,22 @@ import { createGlobalStyle } from "styled-components";
 import "modern-normalize";
 import "react-toastify/dist/ReactToastify.css";
 
-export const GlobalStyle = createGlobalStyle`
+import Poppins700 from "../../fonts/Poppins-Bold.ttf";
+import Poppins600 from "../../fonts/Poppins-SemiBold.ttf";
+import Poppins500 from "../../fonts/Poppins-Medium.ttf";
+import Poppins400 from "../../fonts/Poppins-Regular.ttf";
 
+export const GlobalStyle = createGlobalStyle`
+@font-face {
+font-family: 'Poppins';
+      src: url(${Poppins400}), url(${Poppins500}), url(${Poppins600}), url(${Poppins700});
+    }
 
 body {
    color: ${(p) => p.theme.colors.mainText};
  background-color: ${(p) => p.theme.colors.mainBackground};
   margin: 0;
- font-family: 'Poppins', sans-serif;
+   font-family: 'Poppins', sans-serif;
 
   * {
   transition: color 500ms cubic-bezier(0.075, 0.82, 0.165, 1), background-color 500ms cubic-bezier(0.075, 0.82, 0.165, 1);
